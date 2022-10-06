926414387855eeafccebfb51f2f09bdd4303aa25
aanagn <43435082+aanagn@users.noreply.github.com>
2022-10-06 11:14:53 +0300

    Add libraries import

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
+#Import basic libraries
+import os
 import sys
+import time 
 
 print("Welcome to the playground.")
-print("Python Version:-", sys.version)
+print("Python Version:-", sys.version) # Check the env's version to compare with the docker's
+
+
+# Import data science libraries
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+#import seaborn as sns
+#sns.set_style("whitegrid")
+#plt.style.use("tableau-colorblind10") #https://matplotlib.org/3.1.0/gallery/style_sheets/style_sheets_reference.html
+
+# Import deep learning libraries
+#import tensorflow as tf
+#from tensorflow import keras
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, LSTM, Dropout, Flatten
+#from tensorflow.keras.models import load_model
+
+from sklearn.preprocessing import MinMaxScaler
